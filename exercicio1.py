2732e24c7551b99ff0892313927124a53473f07c
Sérgio Sales <101129874+SALESJR21@users.noreply.github.com>
2025-02-15 10:03:04 -0300

    Cálculo de média
    - Solicitação de entradas do usuário para três notas.
    
    - Cálculo da média das notas.
    
    - Condicional para verificar se o aluno foi aprovado, reprovado ou está de recuperação.
    
    - Mensagens de saída com os resultados.

---
+++ exercicio1.py
@@ -0,0 +1,21 @@
+# Calculo de a médioa do aluno
+
+# Solicitando as notas ao usuário
+
+nota1 = float(input("Informe a nota 1: "))
+nota2 = float(input("Informe a nota 2: "))
+nota3 = float(input("Informe a nota 3: "))
+
+# Calculo da média
+
+media = (nota1 + nota2 + nota3) / 3
+
+# Verificar a média e imprimir o resultado para o(a) usuário(a)
+
+if media >= 7:
+    print("Você foi aprovado(a) com a média: ", media)
+elif media < 5:
+    print("Você foi reprovado(a) com a média ", media)
+else:
+    print("Sua média foi ", media, "e você está de recuperação!")
+
